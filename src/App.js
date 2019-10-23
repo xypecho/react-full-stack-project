1ae98d9c63b9cf5c1d0e63e1a33c70f1e402ff6a
xypecho <xypecho@163.com>
2019-10-23 17:05:12 +0800

    完成内页框架

--- src/App.js
+++ src/App.js
@@ -2,33 +2,17 @@
  * @Author: xueyp
  * @Date: 2019-10-23 14:12:21
  * @Last Modified by: xueyp
- * @Last Modified time: 2019-10-23 15:13:53
+ * @Last Modified time: 2019-10-23 16:50:00
  * @description: 入口文件
  */
 import React from "react";
-import Login from '@/page/login/index.jsx';
-import Layout from '@/layout/index.jsx';
+import Login from './pages/login/index.jsx';
+import LayoutElem from './layout/index.jsx';
 import { BrowserRouter as Router, Route, Link, Redirect, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/" render={props => (
-          <Layout>
-            {/* <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/product" component={ProductRouter} />
-              <Route path="/product-category" component={ProductRouter} />
-              <Route path='/user/index' component={UserList} />
-              <Redirect exact from='/user' to='/user/index' />
-              <Route component={ErrorPage} />
-            </Switch> */}
-          </Layout>
-        )} />
-      </Switch>
-    </Router>
+    <LayoutElem />
   );
 }
 
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,7 +1,18 @@
 /*
  * @Author: xueyp
  * @Date: 2019-10-23 15:17:37
- * @Last Modified by:   xueyp
- * @Last Modified time: 2019-10-23 15:17:37
+ * @Last Modified by: xueyp
+ * @Last Modified time: 2019-10-23 16:49:30
  * @description: 登录页面
  */
+import React from "react";
+
+function Login() {
+    return (
+        <div>
+            这是登录页
+    </div>
+    );
+}
+
+export default Login;
