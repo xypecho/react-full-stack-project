8d2c438a0ffce8717f372043702bf8b073b4b4cd
xypecho <xypecho@163.com>
2019-10-23 15:23:43 +0800

    初始化项目结构

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*
+ * @Author: xueyp
+ * @Date: 2019-10-23 14:12:21
+ * @Last Modified by: xueyp
+ * @Last Modified time: 2019-10-23 15:13:53
+ * @description: 入口文件
+ */
+import React from "react";
+import Login from '@/page/login/index.jsx';
+import Layout from '@/layout/index.jsx';
+import { BrowserRouter as Router, Route, Link, Redirect, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/" render={props => (
+          <Layout>
+            {/* <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/product" component={ProductRouter} />
+              <Route path="/product-category" component={ProductRouter} />
+              <Route path='/user/index' component={UserList} />
+              <Redirect exact from='/user' to='/user/index' />
+              <Route component={ErrorPage} />
+            </Switch> */}
+          </Layout>
+        )} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/login/index.jsx
@@ -0,0 +1,7 @@
+/*
+ * @Author: xueyp
+ * @Date: 2019-10-23 15:17:37
+ * @Last Modified by:   xueyp
+ * @Last Modified time: 2019-10-23 15:17:37
+ * @description: 登录页面
+ */
