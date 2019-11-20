26ca291ac8c3aa2091160cea5256a09db910eb22
xypecho <xypecho@163.com>
2019-11-20 13:22:46 +0800

    update

--- src/pages/fileUpload/detail.jsx
+++ src/pages/fileUpload/detail.jsx
@@ -2,10 +2,13 @@
  * @Author: xueyp
  * @Date: 2019-11-19 14:32:58
  * @Last Modified by: xueyp
- * @Last Modified time: 2019-11-19 15:09:04
+ * @Last Modified time: 2019-11-19 17:10:11
  * @description: 文件上传详情页
  */
 import React from 'react';
+import { Button } from 'antd';
+import Waterfalls from 'component/waterfalls/index.jsx';
+import './index.styl';
 
 export default class FileUploadDetail extends React.Component {
     componentDidMount() {
@@ -15,9 +18,12 @@ export default class FileUploadDetail extends React.Component {
     }
     render() {
         return (
-            <div>
-                呆呆
-            </div>
+            <div className="uploadFileDetail">
+                <div className="back_btn">
+                    <Button size='small' type="primary" onClick={this.props.history.goBack}>返回</Button>
+                </div>
+                <Waterfalls />
+            </div >
         )
     }
 }
